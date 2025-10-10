70e4926a8b4907e4c5b8a453d16ef4e0ff9d2af5
Tonco <toncoivanovic@gmail.com>
2025-10-10 15:50:39 +0200

    api change

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from 'axios';
 
-// const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://admin.chosen-international.com/api';
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://admin.chosen-international.com/api';
+// const API_BASE_URL = 'http://127.0.0.1:8000';
 
 
 /**
